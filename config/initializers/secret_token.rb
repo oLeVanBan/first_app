e49e3913a8308b06978d49864ba2489d932297b2
KakaShi <itachi@ubuntu.ubuntu-domain>
2013-03-16 11:10:37 +0700

    First heroku test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c5e1d84019a350b7d0bbaa06fcccc3cba30c2134e35bff1e77937d04a100189d5dc7fa3ddd822a13402df45fec76f0db5fb75414e2e7cc83018b1f04b9a2deea'
